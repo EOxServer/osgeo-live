bb426f88930926679e44a57fbcecf6b1efdb18bf
Stephan Meißl <stephan.meissl@eox.at>
2012-06-28 10:23:09 +0000

    Working on OSGeo-Live install script.

--- osgeo-live/install_eoxserver.sh
+++ osgeo-live/install_eoxserver.sh
@@ -21,12 +21,23 @@
 # =======
 # sudo ./install_eoxserver.sh
 
+
+echo "Starting EOxServer installation"
+
 if [ -z "$USER_NAME" ] ; then
    USER_NAME="user"
 fi
 USER_HOME="/home/$USER_NAME"
 DATA_DIR="$USER_HOME/gisvm/app-data/eoxserver"
+DOC_DIR="$USER_HOME/gisvm/app-data/eoxserver/doc"
 APACHE_CONF="/etc/apache2/conf.d/eoxserver"
+
+
+## check required tools are installed
+if [ ! -x "`which wget`" ] ; then
+   echo "ERROR: wget is required, please install it and try again" 
+   exit 1
+fi
 
 
 #Install packages
@@ -43,7 +54,7 @@ fi
 
 
 # Install EOxServer
-pip install --upgrade eoxserver
+pip install --upgrade eoxserver==0.2.0
 
 
 # Adjust pysqlite installation (without define=SQLITE_OMIT_LOAD_EXTENSION)
@@ -67,11 +78,15 @@ pip install --upgrade pyspatialite
 [ -d $DATA_DIR ] || mkdir $DATA_DIR
 cd $DATA_DIR
 if [ ! -d eoxserver_demonstration ] ; then
+    echo "Creating EOxServer demonstration instance"
     eoxserver-admin.py create_instance eoxserver_demonstration --init_spatialite
     cd eoxserver_demonstration
+    # Configure logging
+    sed -e 's/#logging_level=/logging_level=INFO/' -i conf/eoxserver.conf
+    sed -e 's/DEBUG = True/DEBUG = False/' -i settings.py
     python manage.py syncdb --noinput
     # Download and register demonstration data
-    wget -c "http://eoxserver.org/export/1718/downloads/EOxServer_autotest-0.2.0.tar.gz" \
+    wget -c "http://eoxserver.org/export/head/downloads/EOxServer_autotest-0.2.0.tar.gz" \
       -O EOxServer_autotest-0.2.0.tar.gz
     echo -n "Extracting demonstration data in `pwd`.\n"
     tar -xzf EOxServer_autotest-0.2.0.tar.gz
@@ -90,11 +105,11 @@ if [ ! -d eoxserver_demonstration ] ; then
     touch logs/eoxserver.log
     chown www-data logs/eoxserver.log data/ data/config.sqlite
     sed -e 's/http_service_url=http:\/\/localhost:8000\/ows/http_service_url=http:\/\/localhost\/eoxserver\/ows/' -i conf/eoxserver.conf
-    
 fi
 
 
-# Deploy instance in Apache2
+# Deploy demonstration instance in Apache2
+echo "Deploying EOxServer demonstration instance"
 if [ ! -e $DATA_DIR/eoxserver_demonstration/wsgi.py ] ; then
     cat << EOF > $DATA_DIR/eoxserver_demonstration/wsgi.py
 import os
@@ -113,15 +128,34 @@ Alias /media /usr/local/lib/python2.7/dist-packages/django/contrib/admin/media
 Alias /static /usr/local/lib/python2.7/dist-packages/eoxserver/webclient/static
 Alias /eoxserver "$DATA_DIR/eoxserver_demonstration/wsgi.py"
 
+################################################################################
+#Restrict wsgi threads in order to run non thread safe code:
+WSGIDaemonProcess ows threads=1
+WSGIProcessGroup ows
+################################################################################
+
 <Directory "$DATA_DIR/eoxserver_demonstration">
     AllowOverride None
     Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
     AddHandler wsgi-script .py
+    ############################################################################
+    #Restrict wsgi threads in order to run non thread safe code:
+    WSGIRestrictProcess ows
+    SetEnv PROCESS_GROUP ows
+    ############################################################################
     Order allow,deny
     allow from all
 </Directory>
 EOF
 echo -n "Done\n"
+
+
+# Install desktop icon
+echo "Installing EOxServer desktop icon"
+if [ ! -e "/usr/share/icons/eoxserver_60x60.logo.png" ] ; then
+   cp "$USER_HOME/gisvm/doc/images/project_logos/logo-eoxserver-3.png" \
+       /usr/share/icons/eoxserver_60x60.logo.png
+fi
 
 
 # Add Launch icon to desktop
@@ -132,15 +166,53 @@ Type=Application
 Encoding=UTF-8
 Name=EOxServer
 Comment=EOxServer
-Categories=Application;Education;Geography;
+Categories=Geospatial;Geoservers;
 Exec=firefox http://localhost/eoxserver/
-Icon=gnome-globe
+Icon=/usr/share/icons/eoxserver_60x60.logo.png
 Terminal=false
 StartupNotify=false
-Categories=Education;Geography;
 EOF
 fi
 cp /usr/share/applications/eoxserver.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME.$USER_NAME "$USER_HOME/Desktop/eoxserver.desktop"
+
+
+# Add menu item
+apt-get --assume-yes install menu
+if [ ! -e /usr/share/menu/eoxserver ] ; then
+   cat << EOF > /usr/share/menu/eoxserver
+?package(eoxserver):needs="X11"\
+  section="Geospatial/Geoservers"\
+  title="EOxServer"\
+  command="firefox http://localhost/eoxserver/"\
+  icon="/usr/share/icons/eoxserver_60x60.logo.png"
+EOF
+  update-menus
+fi
+
+
+# EOxServer Documentation
+echo "Getting EOxServer documentation"
+cd $DOC_DIR
+wget -c "http://eoxserver.org/export/head/downloads/EOxServer_documentation-0.2.0.pdf" \
+  -O EOxServer_documentation-0.2.0.pdf
+# Add Documentation Launch icon to desktop
+if [ ! -e /usr/share/applications/eoxserver-docs.desktop ] ; then
+   cat << EOF > /usr/share/applications/eoxserver-docs.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=EOxServer Documentation
+Comment=EOxServer Documentation
+Categories=Geospatial;Geoservers;
+Exec=evince "$DOC_DIR/EOxServer_documentation-0.2.0.pdf"
+Icon=/usr/share/icons/eoxserver_60x60.logo.png
+Terminal=false
+StartupNotify=false
+EOF
+fi
+cp -a /usr/share/applications/eoxserver-docs.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/eoxserver-docs.desktop"
 
 
 # Reload Apache
@@ -148,4 +220,9 @@ cp /usr/share/applications/eoxserver.desktop "$USER_HOME/Desktop/"
 
 
 # Uninstall dev packages
-#TODO
+apt-get --assume-yes remove libgdal1-dev libsqlite3-dev python2.7-dev \
+    libproj-dev
+apt-get --assume-yes autoremove
+
+
+echo "Finished EOxServer installation"
